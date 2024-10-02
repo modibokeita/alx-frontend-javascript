87a52656e58bb216ade4f4765997df39c523ccc1
modibo keita <93909081keita@gmail.com>
2024-10-02 23:50:08 +0300

    test file

---
+++ 0x01-ES6_promise/0-promise.test.js
@@ -0,0 +1,6 @@
+import getResponseFromAPI from './0-promise.js';
+
+test('getResponseFromAPI returns a Promise', () => {
+  const response = getResponseFromAPI();
+  expect(response).toBeInstanceOf(Promise);
+});
