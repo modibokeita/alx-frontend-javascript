ed2b9e994d2600fffb9b528122a7f52614f133e3
modibo keita <93909081keita@gmail.com>
2024-10-02 23:44:03 +0300

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // Simulate API response by resolving the promise
+      resolve("Success");
+    });
+  }
