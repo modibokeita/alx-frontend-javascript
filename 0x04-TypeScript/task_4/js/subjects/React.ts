1df8abbc027346b55e8ed4fc5aadd1f52203cf14
modibo keita <93909081keita@gmail.com>
2024-10-10 13:42:43 +0300

    fix error

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,15 +1,21 @@
 namespace Subjects {
-    export class React extends Subject {
-      getRequirements(): string {
-        return 'Here is the list of requirements for React';
-      }
 
-      getAvailableTeacher(): string {
-        if (this.teacher?.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
-          return `Available Teacher: ${this.teacher.firstName}`;
-        } else {
-          return 'No available teacher';
-        }
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for React';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher?.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return 'No available teacher';
       }
     }
   }
+}
+
